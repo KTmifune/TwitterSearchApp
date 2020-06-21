4c7b0621a3f0508a22ed66793e0659c1a17f9be3
Takuma Kato <k.t.mifune@gmail.com>
2020-06-21 17:14:31 +0900

    Fix：モデルの変数名変更

--- app/src/main/java/cowma/katotama/twittersearchapp/TweetModel.kt
+++ app/src/main/java/cowma/katotama/twittersearchapp/TweetModel.kt
@@ -5,6 +5,6 @@ import android.graphics.Bitmap
 //TwitterAPIから取得したデータを格納するモデル
 data class TweetModel(
     val userName : String,
-    val comment:String,
-    val iconImage : Bitmap
+    val tweetText :String,
+    val profileImage : Bitmap
 )

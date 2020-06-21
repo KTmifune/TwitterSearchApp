894518223c475f1235612d8fbb00fb5a17e14d56
Takuma Kato <k.t.mifune@gmail.com>
2020-06-21 17:02:53 +0900

    ADD：任意の文字列をキーワードにして、Twitter APIからjsonを取得。１５件のツイートの名前、内容、アイコン画像のurlを取り出しlogに表示

--- app/src/main/java/cowma/katotama/twittersearchapp/TwitterAPIClient.kt
+++ app/src/main/java/cowma/katotama/twittersearchapp/TwitterAPIClient.kt
@@ -1,11 +1,15 @@
 package cowma.katotama.twittersearchapp
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.util.Log
 import android.widget.ListView
 import android.widget.Toast
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.net.URL
+
 
 class TwitterAPIClient(private val context: Context, val listView: ListView)  {
     private val tag = "TwitterAPIClient"
@@ -13,7 +17,8 @@ class TwitterAPIClient(private val context: Context, val listView: ListView)  {
     suspend fun twitterGetTask(vararg keyword: String) {
         try {
 
-            val url = "https://api.twitter.com/1.1/search/tweets.json?q=$keyword&lang=ja"
+            val tweetList<>
+            val url = "https://api.twitter.com/1.1/search/tweets.json?q=${keyword[0]}&lang=ja"
             val http = HttpUtil()
 
             // onPreExecute
@@ -21,20 +26,44 @@ class TwitterAPIClient(private val context: Context, val listView: ListView)  {
                 Toast.makeText(context,"start : $keyword",Toast.LENGTH_SHORT).show()
             }
 
-            // doInBackgroundメソッド
+            // API
             val response = http.httpGet(url)
+            // Json
+            val jsonObj = JSONObject(response)
+            // Get json List
+            val dataList = jsonObj.getJSONArray("statuses")
 
-            Log.e(tag,"response$response")
+            //
+//            Log.d(tag,"data->$dataList")
+//            Log.d(tag,"data.length${dataList.length()}")
 
-            //val result = http.httpGet(uri)
+            for (i in 0 until dataList.length()) {
+                val data = dataList.getJSONObject(i)
+                // get name
+                val name = data.getJSONObject("user").getString("name")
+                // get text
+                val text = data.getString("text")
+
+                val imageUrlHttps = data.getJSONObject("user").getString("profile_image_url_https")
+
+                Log.d(tag,"Name:$name Text:$text URl:$imageUrlHttps\n")
+
+                //Get image bitmap
+                val imageUrl = URL(imageUrlHttps)
+                val imageIs = imageUrl.openStream()
+                val iconImage = BitmapFactory.decodeStream(imageIs)
+
+
+
+            }
 
             // onPostExecuteメソッド
-            withContext(Dispatchers.Main) {
-                Toast.makeText(context,"finish",Toast.LENGTH_SHORT).show()
-            }
+//            withContext(Dispatchers.Main) {
+//                Toast.makeText(context,"finish",Toast.LENGTH_SHORT).show()
+//            }
         } catch (e: Exception) {
             // onCancelledメソッド
-            Log.e(tag, "Cancel", e)
+            Log.e(tag, "Error In TwitterAPIClient", e)
         }
     }
 }
